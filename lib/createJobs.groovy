1d42c88860bce5d6f2e7a9bf57a163001564d3dc
vijaylondhe <vijay.londhe815@gmail.com>
2022-01-29 13:26:11 +0530

    changed branch name to main in seedJob.xml

--- lib/createJobs.groovy
+++ lib/createJobs.groovy
@@ -1,7 +1,7 @@
 pipelineJob('pipelineJob') {
     definition {
         cps {
-            script(readFileFromWorkspace('lib/pipelineJob.groovy'))
+            script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
         }
     }
